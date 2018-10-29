a2329dbac2395aeea4f4bc8dff5664ec1f542ef5
Tiago Pereira <tiagomrp06@gmail.com>
2018-10-29 20:18:57 +0000

    01

--- src/isel/poo/snake/view/StatusPanel.java
+++ src/isel/poo/snake/view/StatusPanel.java
@@ -5,26 +5,30 @@ import isel.poo.console.FieldView;
 import isel.poo.console.View;
 
 public class StatusPanel extends View {
-    public static final int HEIGHT=0,WIDTH = 0;
-    public int apples;
-    public int score;
-    public int level;
-
+    public static int HEIGHT = 10, WIDTH = 8;
+    private FieldView Level = new FieldView("Level", 1, 0, "---");
+    private FieldView Score = new FieldView("Score", 4, 0, "---");
+    private FieldView Apples = new FieldView("Apples", 7, 0, "---");
 
     public StatusPanel(int winWidth) {
+        super(0, winWidth, HEIGHT, WIDTH, Console.LIGHT_GRAY);
+        WIDTH = winWidth;
+        addView(Level);
+        addView(Score);
+        addView(Apples);
 
     }
 
 
     public void setApples(int apples) {
-        this.apples=apples;
+        this.Apples.setValue(apples);
     }
 
     public void setScore(int score) {
-        this.score=score;
+        this.Score.setValue(score);
     }
 
-    public void setLevel(int number) {
-        this.level=level;
+    public void setLevel(int level) {
+        this.Level.setValue(level);
     }
 }
